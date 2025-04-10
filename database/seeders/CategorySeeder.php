c34a0c604b153898e8c269d615492c5790920e15
Jerome Morales <194875286+morales-lc@users.noreply.github.com>
2025-04-10 17:10:19 +0800

    first commit, /products and /categories routes

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $categories = ['Electronics', 'Books', 'Clothing', 'Home Appliances'];
+
+        foreach ($categories as $name) {
+            Category::create(['name' => $name]);
+        }
+    }
+}
